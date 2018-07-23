446efb37ff227f430e3394dd71515c29af706611
Danielle888 <41483614+Danielle888@users.noreply.github.com>
2018-07-23 15:18:56 -0500

    Add files via upload

---
+++ workingsnake.py
@@ -0,0 +1,189 @@
+from turtle import *
+import random
+import time
+
+SIZE = 20
+
+class Part(RawTurtle):
+    def __init__(self, x, y, screen):
+        self.x = x
+        self.y = y
+        self.screen = screen
+        
+    def drawSelf(self, turtle):
+        turtle.goto(self.x - SIZE // 2 -1, self.y - SIZE // 2 -1)
+        if random.random() < 0.5:
+            turtle.color("blue")
+        else:
+            turtle.color("black")
+        turtle.begin_fill()
+        for _ in range(4):
+            turtle.forward(SIZE - SIZE // 10)
+            turtle.left(90)
+        turtle.end_fill()
+        
+class Apple:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def changeLocation(self):
+        self.x = random.randint(0, SIZE) * SIZE - 200
+        self.y = random.randint(0, SIZE) * SIZE - 200
+        
+    def drawSelf(self, turtle):
+        turtle.goto(self.x - SIZE // 2 - 1, self.y - SIZE // 2 -1)
+        turtle.color("red")
+        turtle.begin_fill()
+        for _ in range(4):
+            turtle.forward(SIZE - SIZE // 10)
+            turtle.left(90)
+        turtle.end_fill()
+            
+        
+class Snake:
+    def __init__(self, screen):
+        self.headPosition = [SIZE, 0]
+        self.screen = screen
+        self.body = [Part(-SIZE, 0, self.screen), Part(0,0, self.screen), Part(SIZE, 0, self.screen)]
+        self.nextX = 1
+        self.nextY = 0
+        self.crashed = False
+        self.nextposition = [self.headPosition[0] + SIZE * self.nextX, self.headPosition[1] + SIZE * self.nextY]
+    
+    def moveOneStep(self):
+        self.body.append(Part(self.nextposition[0], self.nextposition[1], self.screen)) 
+        del self.body[0]
+        self.headPosition[0], self.headPosition[1] = self.body[-1].x, self.body[-1].y 
+        self.nextposition = [self.headPosition[0] + SIZE * self.nextX, self.headPosition[1] + SIZE * self.nextY]
+            
+    def testCollision(self):
+        for i in range(len(self.body)-1):
+            if self.nextposition[0] == self.body[i].x and self.nextposition[1] == self.body[i].y:
+                self.crashed = True
+        if self.nextposition[0] < -300 or self.nextposition[0] > 300 or self.nextposition[1] > 300 or self.nextposition[1] < -300:
+            self.crashed = True
+                    
+            
+    def moveUp(self):
+        if self.nextX != 0 and self.nextY != -1:
+            self.nextX, self.nextY = 0, 1
+    def moveLeft(self):
+        if self.nextX != 1 and self.nextY != 0:
+            self.nextX, self.nextY = -1, 0
+    def moveRight(self):
+        if self.nextX != -1 and self.nextY != 0:
+            self.nextX, self.nextY = 1, 0
+    def moveDown(self):
+        if self.nextX != 0 and self.nextY != 1:
+            self.nextX, self.nextY = 0, -1
+    def eatApple(self):
+        self.body.append(Part(self.nextposition[0], self.nextposition[1], self.screen))
+        self.headPosition[0], self. headPosition[1] = self.body[-1].x, self.body[-1].y
+        self.nextPositon = [self.headPosition[0] + SIZE * self.nextX, self.headPosition[1] + SIZE * self.nextY]
+
+    def drawSelf(self, turtle):
+        if not self.crashed:
+            for part in self.body:
+                part.drawSelf(turtle)
+        if self.crashed:
+            if self.nextposition[0] > 250:
+                turtle.write("Game Over!", move = False, align = "right", font = ("Arial", 20, "normal"))
+                turtle.goto(turtle.xcor(), turtle.ycor()- 12)
+                turtle.write("Press Space to play again!", move = False, align = "right", font = ("Arial", 20, "normal"))
+            elif self.nextposition[0] < -250:
+                turtle.write("Game Over!", move = False, align = "left", font = ("Arial", 20, "normal"))
+                turtle.goto(turtle.xcor(), turtle.ycor()- 12)
+                turtle.write("Press Space to play again!", move = False, align = "left", font = ("Arial", 20, "normal"))
+            elif self.nextposition[1] < -250:
+                turtle.goto(turtle.xcor(), turtle.ycor()+ 14)
+                turtle.write("Game Over!", move = False, align = "center", font = ("Arial", 20, "normal"))
+                turtle.goto(turtle.xcor(), turtle.ycor()- 12)
+                turtle.write("Press Space to play again!", move = False, align = "center", font = ("Arial", 20, "normal"))                
+            else:
+                turtle.write("Game Over!", move = False, align = "center", font = ("Arial", 20, "normal"))
+                turtle.goto(turtle.xcor(), turtle.ycor()- 12)
+                turtle.write("Press Space to play again!", move = False, align = "center", font = ("Arial", 20, "normal"))
+            
+class Game():
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.setworldcoordinates(-300, -300, 300, 300)
+        self.artist = Turtle(visible = False)
+        self.artist.up()
+        self.artist.speed("slowest")
+        
+        self.snake = Snake(self.screen)
+        self.apple = Apple(100,0)
+        self.commandpending = False
+        
+        self.screen.tracer(0)
+       
+        
+        self.screen.listen()
+        self.screen.onkey(self.snakeDown, "Down")
+        self.screen.onkey(self.snakeUp, "Up")
+        self.screen.onkey(self.snakeLeft, "Left")
+        self.screen.onkey(self.snakeRight, "Right")
+        
+        self.screen.onkey(self.snakeDown, "s")
+        self.screen.onkey(self.snakeUp, "w")
+        self.screen.onkey(self.snakeLeft, "a")
+        self.screen.onkey(self.snakeRight, "d")
+        
+        
+    def nextFrame(self):
+        self.artist.clear()
+        if not self.snake.crashed:
+            if (self.snake.nextposition[0], self.snake.nextposition[1]) == (self.apple.x, self.apple.y):
+                self.snake.eatApple()
+                self.apple.changeLocation()
+            else:
+                self.snake.moveOneStep()
+            
+            self.apple.drawSelf(self.artist)
+            self.snake.drawSelf(self.artist)
+            self.screen.update()
+            self.screen.ontimer(lambda: self.nextFrame(), 100)      
+            self.snake.testCollision()
+        else:
+            self.snake.drawSelf(self.artist)
+            self.screen.onkey(self.newGame, "space")
+        
+    def snakeUp(self):
+        if not self.commandpending: 
+            self.commandpending = True
+            self.snake.moveUp()
+            self.commandpending = False
+    
+    def snakeDown(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveDown()
+            self.commandpending = False
+    
+    def snakeLeft(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveLeft()
+            self.commandpending = False
+    
+    def snakeRight(self):
+        if not self.commandpending:
+            self.commandpending = True
+            self.snake.moveRight()
+            self.commandpending = False
+            
+    def newGame(self):
+        self.snake = Snake(self.screen)
+        self.apple = Apple(100,0)
+        self.commandpending = False
+        self.nextFrame()    
+    
+game = Game()
+        
+screen = Screen()
+        
+screen.ontimer(lambda: game.nextFrame(), 100)
+        
+screen.mainloop() 
